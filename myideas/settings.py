0696fa91f924ff2b293d94adf65f7fbc0f670f74
Diego Ubirajara <diego.ubirajara@mail.com>
2016-12-25 15:05:48 +0100

    update

--- myideas/settings.py
+++ myideas/settings.py
@@ -28,6 +28,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+DEFAULT_FROM_EMAIL = 'contact@shareideas.com.br'
+
 # Application definition
 
 INSTALLED_APPS = [
