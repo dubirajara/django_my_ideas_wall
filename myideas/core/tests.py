1033b48a2fb33b4ec3d73b21be44c6eabd0bcfb5
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-02 19:28:26 +0100

    add test

--- myideas/core/tests.py
+++ myideas/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET 'Home' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """'Home' must use template index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
+
