25494c4c2ff49ad7eef85b9d892b061c7423d733
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-03 19:07:26 +0100

    add more tests

--- myideas/core/tests.py
+++ myideas/core/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
 
     def setUp(self):
-        self.response = self.client.get('/')
+        self.response = self.client.get(r('home'))
 
     def test_get(self):
         """GET 'Home' must return status code 200"""
@@ -14,3 +15,17 @@ class HomeTest(TestCase):
         """'Home' must use template index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
+    def test_login_link(self):
+        """'Home' must contains link to login page"""
+        expected = 'href="{}"'.format(r('auth_login'))
+        self.assertContains(self.response, expected)
+
+    def test_register_link(self):
+        """'Home' must contains link to register page"""
+        expected = 'href="{}"'.format(r('registration_register'))
+        self.assertContains(self.response, expected)
+
+    def test_ideas_form_link(self):
+        """'Home' must contains link to register page"""
+        expected = 'href="{}"'.format(r('ideas_form'))
+        self.assertContains(self.response, expected)
