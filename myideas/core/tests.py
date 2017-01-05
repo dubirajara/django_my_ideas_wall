c36bdea567ffd009da74dbff57116893b88d4351
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-05 21:37:45 +0100

    add more tests

--- myideas/core/tests.py
+++ myideas/core/tests.py
@@ -6,7 +6,6 @@ from .models import Ideas
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create(username='adminapp')
         self.response = self.client.get(r('home'))
 
     def test_get(self):
@@ -35,8 +34,8 @@ class HomeTest(TestCase):
 
 class DetailsTest(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create(username='adminapp')
-        self.idea = Ideas.objects.create(user=self.user, title='test app')
+        user = get_user_model().objects.create(username='adminapp')
+        self.idea = Ideas.objects.create(user=user, title='test app')
         self.response = self.client.get(r(self.idea.get_absolute_url()))
 
     def test_get_absolute_url(self):
@@ -46,3 +45,22 @@ class DetailsTest(TestCase):
     def test_get_details(self):
         """GET 'Ideas Details' must return status code 200"""
         self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """'Ideas Details' must use template ideas_details.html"""
+        self.assertTemplateUsed(self.response, 'ideas_details.html')
+
+    def test_login_link(self):
+        """'Home' must contains link to login page"""
+        expected = 'href="{}"'.format(r('auth_login'))
+        self.assertContains(self.response, expected)
+
+    def test_register_link(self):
+        """'Home' must contains link to register page"""
+        expected = 'href="{}"'.format(r('registration_register'))
+        self.assertContains(self.response, expected)
+
+    def test_ideas_form_link(self):
+        """'Home' must contains link to register page"""
+        expected = 'href="{}"'.format(r('ideas_form'))
+        self.assertContains(self.response, expected)
