b40c5c8a0ecf0c8630a88ac6a9e3cc6cb35065f1
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-08 14:47:26 +0100

    refactor urls

--- myideas/urls.py
+++ myideas/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from myideas.core import views
 
 urlpatterns = [
