64ae5f038ffd0e949c7321455e7b1c0d85aadb28
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-14 16:59:08 +0100

    update tests

--- myideas/core/tests/test_forms.py
+++ myideas/core/tests/test_forms.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
-from myideas.core.forms import IdeasForm
+from myideas.core.forms import IdeasForm, IdeasFormUpdate
 
 
 class IdeasFormTest(TestCase):
-
     def test_form_has_fields(self):
         """Form must have 3 fields"""
         form = IdeasForm()
         expected = ('title', 'description', 'tags')
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+
+class IdeasFormUpdateTest(TestCase):
+
+    def test_form_has_fields(self):
+        """Form must have 2 fields"""
+        form = IdeasFormUpdate()
+        expected = ('title', 'description')
         self.assertSequenceEqual(expected, list(form.fields))
 
     def assertFormErrorCode(self, form, field, code):
