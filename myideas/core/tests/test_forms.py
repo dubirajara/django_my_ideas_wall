231b253dbcfae17213cd39e392f8b373431b3189
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-14 16:57:15 +0100

    update tests

--- myideas/core/tests/test_forms.py
+++ myideas/core/tests/test_forms.py
@@ -7,5 +7,11 @@ class IdeasFormTest(TestCase):
     def test_form_has_fields(self):
         """Form must have 3 fields"""
         form = IdeasForm()
-        expected = ['title', 'description', 'tags']
+        expected = ('title', 'description', 'tags')
         self.assertSequenceEqual(expected, list(form.fields))
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
