d9c8248e919ef1d3377ff5b9c6e95252dbff5f31
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-15 08:27:54 +0100

    update tests

--- myideas/core/tests/test_forms.py
+++ myideas/core/tests/test_forms.py
@@ -4,14 +4,16 @@ from myideas.core.forms import IdeasForm, IdeasFormUpdate
 
 class IdeasFormTest(TestCase):
 
+    def setUp(self):
+        self.form = IdeasForm()
+
     def test_form_has_fields(self):
-        """Form must have 3 fields"""
-        form = IdeasForm()
+        """IdeasForm must have 3 fields"""
         expected = ('title', 'description', 'tags')
-        self.assertSequenceEqual(expected, list(form.fields))
+        self.assertSequenceEqual(expected, list(self.form.fields))
 
     def test_all_required_form_fields(self):
-        """Test form field is required."""
+        """Test Ideasform field is required."""
         form = IdeasForm({
             'title': '',
             'description': '',
@@ -20,17 +22,25 @@ class IdeasFormTest(TestCase):
         self.assertIn('title', form.errors)
         self.assertIn('description', form.errors)
 
+    def test_fields_not_present(self):
+        """Test Ideasform field is not present."""
+        self.assertFalse(self.form.fields.get('created_at'))
+        self.assertFalse(self.form.fields.get('slug'))
+        self.assertFalse(self.form.fields.get('user'))
+
 
 class IdeasFormUpdateTest(TestCase):
 
+    def setUp(self):
+        self.form = IdeasFormUpdate()
+
     def test_form_has_fields(self):
-        """Form must have 2 fields"""
-        form = IdeasFormUpdate()
+        """UpdateForm must have 2 fields"""
         expected = ('title', 'description')
-        self.assertSequenceEqual(expected, list(form.fields))
+        self.assertSequenceEqual(expected, list(self.form.fields))
 
     def test_all_required_form_fields(self):
-        """Test form field is required."""
+        """Test Updateform field is required."""
         form = IdeasFormUpdate({
             'title': '',
             'description': '',
@@ -38,3 +48,10 @@ class IdeasFormUpdateTest(TestCase):
         form.is_valid()
         self.assertIn('title', form.errors)
         self.assertIn('description', form.errors)
+
+    def test_fields_not_present(self):
+        """Test Updateform field is not present."""
+        self.assertFalse(self.form.fields.get('user'))
+        self.assertFalse(self.form.fields.get('slug'))
+        self.assertFalse(self.form.fields.get('created_at'))
+        self.assertFalse(self.form.fields.get('tags'))
