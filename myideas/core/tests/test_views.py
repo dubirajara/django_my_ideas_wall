fd366e245b4e9ac23c0228c5b52166333603f107
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-17 17:57:10 +0100

    update test

--- myideas/core/tests/test_views.py
+++ myideas/core/tests/test_views.py
@@ -112,6 +112,7 @@ class IdeasDetailNotFound(TestCase):
 class RegisterUserAdmin(TestCase):
 
     def test_registration(self):
+        """GET registration must return status code 200 and use template base.html"""
         response = self.client.get(r('registration_register'))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response,
@@ -119,6 +120,7 @@ class RegisterUserAdmin(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
     def test_login(self):
+        """GET login must return status code 200 and use template base.html"""
         response = self.client.get(r('auth_login'))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response,
@@ -126,6 +128,7 @@ class RegisterUserAdmin(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
     def test_logout(self):
+        """GET logout must return status code 200 and use template base.html"""
         response = self.client.get(r('auth_logout'))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response,
@@ -133,6 +136,7 @@ class RegisterUserAdmin(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
     def test_get_and_templates_reset_complete(self):
+        """GET pass reset must return status code 200 and use template base.html"""
         response = self.client.get(r('auth_password_reset_complete'))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response,
