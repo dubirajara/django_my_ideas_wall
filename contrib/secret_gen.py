24794064a2e00cd07c130d420ae1a5f1963854a7
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-20 00:13:06 +0100

    update contrib

--- contrib/secret_gen.py
+++ contrib/secret_gen.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """
 Django SECRET_KEY generator.
 """
@@ -6,4 +7,35 @@ from django.utils.crypto import get_random_string
 
 
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-print(get_random_string(50, chars))
+
+CONFIG_STRING = """
+SECRET_KEY={}
+DEBUG=True
+ALLOWED_HOSTS=*
+
+#EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend
+#EMAIL_HOST=localhost
+#EMAIL_HOST_USER=
+#EMAIL_HOST_PASSWORD=
+#EMAIL_PORT=25
+#EMAIL_USE_TLS=False
+
+EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
+EMAIL_HOST=smtp.sendgrid.com
+SENDGRID_API_KEY=
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+
+
+# DJANGO SOCIAL-AUTH SETTINGS
+SOCIAL_AUTH_FACEBOOK_KEY=
+SOCIAL_AUTH_FACEBOOK_SECRET=
+SOCIAL_AUTH_TWITTER_KEY=
+SOCIAL_AUTH_TWITTER_SECRET=
+""".strip().format(get_random_string(50, chars))
+
+# Writing our configuration file to '.env'
+with open('myideas/.env', 'w') as configfile:
+    configfile.write(CONFIG_STRING)
