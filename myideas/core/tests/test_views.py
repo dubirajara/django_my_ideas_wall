5a4ec7c992b9aabbaf45d4b1c427bc1e02214e41
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-20 18:51:07 +0100

    add more tests

--- myideas/core/tests/test_views.py
+++ myideas/core/tests/test_views.py
@@ -1,13 +1,23 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.shortcuts import resolve_url as r
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from myideas.core.forms import IdeasForm
 from myideas.core.models import Ideas
 
 
 class DetailsTest(TestCase):
     def setUp(self):
-        user = get_user_model().objects.create(username='diego')
+        self.client = Client()
+        self.username = 'diego'
+        self.email = 'test@djangoapp.com'
+        self.password = 'test'
+        user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+        self.login = self.client.login(
+            username=self.username, password=self.password
+        )
         self.idea = Ideas.objects.create(
             user=user, title='test app'
         )
@@ -22,6 +32,16 @@ class DetailsTest(TestCase):
         self.assertTemplateUsed(self.response, 'ideas_details.html')
         self.assertTemplateUsed(self.response, 'base.html')
 
+    def test_update_and_delete_link(self):
+        """Details contains update/delete links"""
+        contents = [
+            'href="{}"'.format(r('update', self.idea.slug)),
+            'href="{}"'.format(r('delete', self.idea.slug)),
+                    ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
     def test_html(self):
         self.assertContains(self.response, self.idea.user)
         self.assertContains(self.response, self.idea.title)
@@ -35,7 +55,16 @@ class DetailsTest(TestCase):
 
 class ProfileTest(TestCase):
     def setUp(self):
-        user = get_user_model().objects.create(username='diego')
+        self.client = Client()
+        self.username = 'diego'
+        self.email = 'test@djangoapp.com'
+        self.password = 'test'
+        user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+        self.login = self.client.login(
+            username=self.username, password=self.password
+        )
         self.idea = Ideas.objects.create(user=user)
         self.response = self.client.get(r('profile', self.idea.user))
 
@@ -48,13 +77,23 @@ class ProfileTest(TestCase):
         self.assertTemplateUsed(self.response, 'profile.html')
         self.assertTemplateUsed(self.response, 'base.html')
 
+    def test_update_and_delete_link(self):
+        """Details contains update/delete links"""
+        contents = [
+            'href="{}"'.format(r('update', self.idea.slug)),
+            'href="{}"'.format(r('delete', self.idea.slug)),
+                    ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
     def test_html(self):
         self.assertContains(self.response, self.idea.user)
 
 
 class IdeaTagsTest(TestCase):
     def setUp(self):
-        user = get_user_model().objects.create(username='diego')
+        user = User.objects.create_user(username='diego')
         self.idea = Ideas.objects.create(
             user=user, title='test app', tags='django'
         )
@@ -89,6 +128,37 @@ class IdeaFormTest(TestCase):
     def test_template(self):
         """'Ideas Form' must use template index.html and base.html"""
         self.assertTemplateUsed(self.response, 'idea_form.html')
+        self.assertTemplateUsed(self.response, 'base.html')
+
+
+class IdeasUpdateform(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = 'diego'
+        self.email = 'test@djangoapp.com'
+        self.password = 'test'
+        user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+        self.login = self.client.login(
+            username=self.username, password=self.password
+        )
+        self.idea = Ideas.objects.create(
+            user=user, title='test app'
+        )
+        self.response = self.client.get(r('update', self.idea.slug))
+
+    def test_login(self):
+        """GET 'update Form' must be authenticated and be own post"""
+        self.assertEqual(self.login, True)
+
+    def test_get(self):
+        """GET 'update Form' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """'ideas Update' must use template update.html and base.html"""
+        self.assertTemplateUsed(self.response, 'update.html')
         self.assertTemplateUsed(self.response, 'base.html')
 
 
