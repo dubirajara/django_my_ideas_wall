4a026c5d4a1781a39fea607842d5532c51dac49d
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-22 12:40:03 +0100

    update

--- myideas/wsgi.py
+++ myideas/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
+
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myideas.settings")
 
