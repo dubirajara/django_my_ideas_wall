30da7ee68e3875c760a6c4957cc68f26c8b34ea9
Diego Ubirajara <diego.ubirajara@mail.com>
2018-02-11 09:49:06 +0100

    add more tespi

--- myideas/api/test.py
+++ myideas/api/test.py
@@ -26,6 +26,16 @@ class ListApiTest(APITestCase):
         """GET 'Ideas list api' must return status code 200"""
         self.assertEqual(status.HTTP_200_OK, self.response.status_code)
 
+    def test_api_create(self):
+        """Check created data in db"""
+        self.assertEqual(Ideas.objects.count(), 1)
+
+    def test_api_data(self):
+        """check retieve data in db"""
+        self.assertEqual(Ideas.objects.get().user.username, self.username)
+        self.assertEqual(Ideas.objects.get().title, 'test app')
+        self.assertEqual(Ideas.objects.get().user.email, self.email)
+
     def test_api_html(self):
         """Test returns json contents"""
         contents = (self.idea.user.username, self.idea.likes.count(),
@@ -35,13 +45,26 @@ class ListApiTest(APITestCase):
             for expected in contents:
                 self.assertContains(self.response, expected)
 
-
     def test_api_id_get(self):
         """GET 'Ideas id list api' must return status code 200"""
         self.response.id = self.client.get(r('id_api', self.idea.id))
         self.assertEqual(status.HTTP_200_OK, self.response.id.status_code)
 
     def test_id_html(self):
+        """Test returns json contents"""
+        contents = (self.idea.user.username, self.idea.likes.count(),
+                    self.idea.title)
+
+        with self.subTest():
+            for expected in contents:
+                self.assertContains(self.response, expected)
+
+    def test_api_user_get(self):
+        """GET 'Ideas id list user' must return status code 200"""
+        self.response.user = self.client.get(r('user_api', self.idea.user.username))
+        self.assertEqual(status.HTTP_200_OK, self.response.user.status_code)
+
+    def test_user_html(self):
         """Test returns json contents"""
         contents = (self.idea.user.username, self.idea.likes.count(),
                     self.idea.title)
