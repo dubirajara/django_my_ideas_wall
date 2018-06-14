1cade4144e96c871bf4e113b12fcc06f1ecea5c6
Diego Ubirajara <diego.ubirajara@mail.com>
2018-06-14 19:29:56 +0200

    add whitenoise to server staticfiles

--- myideas/wsgi.py
+++ myideas/wsgi.py
@@ -10,9 +10,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-from dj_static import Cling
+# from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myideas.settings")
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
+# application = Cling(get_wsgi_application())
