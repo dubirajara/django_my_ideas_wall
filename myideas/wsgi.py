6c11cc16f8381f4bfea7cf99d135e65a98c90f30
Diego Ubirajara <diego.ubirajara@mail.com>
2018-08-19 17:43:12 +0200

    update whitenoise, fix it wsgi.py

--- myideas/wsgi.py
+++ myideas/wsgi.py
@@ -10,11 +10,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-# from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myideas.settings")
 
-application = DjangoWhiteNoise(get_wsgi_application())
-# application = Cling(get_wsgi_application())
+application = get_wsgi_application()
