ad64d5edad782d07db0abbeaa68326c6aa3f3382
Diego Ubirajara <diego.ubirajara@mail.com>
2018-11-09 12:36:25 +0100

    improve register and login form test

--- myideas/core/tests/test_views_register.py
+++ myideas/core/tests/test_views_register.py
@@ -41,6 +41,40 @@ class RegisterUserAdmin(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
 
+class LoginRegisterform(TestCase):
+    def test_html_login(self):
+        response = self.client.get(r('auth_login'))
+        contents = (
+            'href="/oauthlogin/facebook/"',
+            'href="/oauthlogin/twitter/"',
+            '<input type="text" name="username"',
+            '<input type="password" name="password"',
+            'href="/accounts/password/reset/"',
+            'href="/accounts/register/"',
+        )
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(response, expected)
+
+    def test_html_register(self):
+        response = self.client.get(r('registration_register'))
+        contents = (
+            'href="/oauthlogin/facebook/"',
+            'href="/oauthlogin/twitter/"',
+            '<input type="text" name="username"',
+            '<input type="email" name="email"',
+            '<input type="password" name="password1"',
+            '<input type="password" name="password2',
+            'id="recaptcha-submit"',
+            '<a href="/accounts/login/"',
+        )
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(response, expected)
+
+
 class RecaptchaForm(Form):
     captcha = fields.ReCaptchaField()
 
