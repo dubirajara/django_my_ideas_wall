e68bfdf67290b74c10ca5d6ebfe670e9ab235ee1
Diego Ubirajara <diego.ubirajara@mail.com>
2018-11-09 14:52:40 +0100

    add test api CRUD

--- myideas/api/test.py
+++ myideas/api/test.py
@@ -7,20 +7,21 @@ from rest_framework.test import APITestCase, APIClient
 from myideas.core.models import Idea
 
 
-class ListApiTest(APITestCase):
+class ApiTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.username = 'diego'
         self.email = 'test@djangoapp.com'
         self.password = 'test'
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
             self.username, self.email, self.password
         )
         self.idea = Idea.objects.create(
-            user=user, title='test app'
+            user=self.user, title='test app'
         )
 
         self.response = self.client.get(r('list_api'))
+        self.client.login(username=self.username, password=self.password)
 
     def test_api_get(self):
         """GET 'Idea list api' must return status code 200"""
@@ -31,7 +32,7 @@ class ListApiTest(APITestCase):
         self.assertEqual(Idea.objects.count(), 1)
 
     def test_api_data(self):
-        """check retieve data in db"""
+        """check retrieve data in db"""
         self.assertEqual(Idea.objects.get().user.username, self.username)
         self.assertEqual(Idea.objects.get().title, 'test app')
         self.assertEqual(Idea.objects.get().user.email, self.email)
@@ -75,3 +76,56 @@ class ListApiTest(APITestCase):
         with self.subTest():
             for expected in contents:
                 self.assertContains(self.response, expected)
+
+    def test_post_idea(self):
+        """POST 'Idea list api' must return status code 201"""
+        response = self.client.post(r('list_api'),
+                                    {'title': 'new idea', 'description': 'new idea', 'slug': 'idea'},
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Idea.objects.count(), 2)
+        self.assertEqual(response.data.get('title'), 'new idea')
+
+    def test_patch_idea(self):
+        """PATCH 'Idea ID api' must return status code 200"""
+        response = self.client.patch(r('id_api', self.idea.id),
+                                     {'title': 'new idea NEW PATCH'},
+                                     format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Idea.objects.count(), 1)
+        self.assertEqual(Idea.objects.get().title, 'new idea NEW PATCH')
+
+    def test_put_idea(self):
+        """PUT 'Idea ID api' must return status code 200"""
+        response = self.client.put(r('id_api', self.idea.id),
+                                   {'title': 'new idea NEW PUT', 'description': 'new idea', 'slug': 'idea'},
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Idea.objects.count(), 1)
+        self.assertEqual(Idea.objects.get().title, 'new idea NEW PUT')
+
+    def test_delete_idea(self):
+        """DELETE 'Idea ID api' must return status code 200"""
+        response = self.client.delete(r('id_api', self.idea.id))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Idea.objects.count(), 0)
+
+    def test_logout_no_permissions(self):
+        """If user not login and try create, update and delete must return status code 403"""
+        self.client.logout()
+        response = (
+            self.client.post(r('list_api'),
+                             {'title': 'new idea', 'description': 'new idea', 'slug': 'idea'},
+                             format='json'),
+            self.client.patch(r('id_api', self.idea.id),
+                              {'title': 'new idea NEW PATCH'},
+                              format='json'),
+            self.client.put(r('id_api', self.idea.id),
+                            {'title': 'new idea NEW PUT', 'description': 'new idea', 'slug': 'idea'},
+                            format='json'),
+            self.client.delete(r('id_api', self.idea.id)),
+            )
+
+        with self.subTest():
+            for expected in response:
+                self.assertEqual(expected.status_code, status.HTTP_403_FORBIDDEN)
